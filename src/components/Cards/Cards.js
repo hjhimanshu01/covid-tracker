c4ed54ff1d6f0f327314f3cb0576f35e0f399711
Himanshu Jotwani <hjhimanshu01@gmail.com>
2020-07-07 13:38:26 +0530

    bug fixes

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -3,101 +3,93 @@ import CountUp from "react-countup";
 import casesImg from "../../images/cases.png";
 import deathsImg from "../../images/deaths-large.png";
 import recoveredImg from "../../images/recovered.png";
+import Moment from "react-moment";
 
 const Cards = ({ data }) => {
   return (
     <div className="landing">
       <div className="container">
-        <div className="row py-5 justify-content-between align-items-center global">
-          <div className="col-md-12">
-            <div className="row justify-content-between align-items-center">
-              <div className="col-sm-6">
-                <h3>Global information : </h3>
+        <div className="col-md-12">
+          <div className="row justify-content-between align-items-center">
+            <div className="col-sm-6">
+              <h3>Global information : </h3>
+            </div>
+            <div className="col-sm-6 text-sm-right"></div>
+          </div>
+        </div>
+        <div className="col-lg-8 text-center my-3"></div>
+        <div className="row">
+          <div className="col-md-6 col-sm-12 col-lg-12 my-3">
+            <div className="row cases shadow align-items-center justify-content-between no-gutters">
+              <div className="col-3">
+                <img src={casesImg} alt="Cases" className="img-fluid logo" />
               </div>
-              <div className="col-sm-6 text-sm-right">
-                <h5></h5>
+              <div className="col-8">
+                <h2 className="text-info">
+                  {data?.cases ? (
+                    <CountUp
+                      start={0}
+                      end={data?.cases}
+                      duration={2.3}
+                      separator={","}
+                    />
+                  ) : (
+                    <small className="text-primary">Loading . . .</small>
+                  )}
+                </h2>
+                <p className="text-muted">Confirmed</p>
               </div>
             </div>
-          </div>
-          <div className="col-lg-8 text-center my-3"></div>
-          <div className="col-lg-4 my-3">
-            <div className="row">
-              <div className="col-md-6 col-sm-12 col-lg-12 my-3">
-                <div className="row cases shadow align-items-center justify-content-between no-gutters">
-                  <div className="col-3">
-                    <img
-                      src={casesImg}
-                      alt="Cases"
-                      className="img-fluid logo"
-                    />
-                  </div>
-                  <div className="col-8">
-                    <h2 className="text-info">
-                      {data?.cases ? (
-                        <CountUp
-                          start={0}
-                          end={data?.cases}
-                          duration={2.3}
-                          separator={","}
-                        />
-                      ) : (
-                        <small className="text-primary">Loading . . .</small>
-                      )}
-                    </h2>
-                    <p className="text-muted">Confirmed</p>
-                  </div>
+
+            <div className="col-md-6 col-sm-12 col-lg-12 my-3">
+              <div className="row deaths shadow align-items-center justify-content-between no-gutters">
+                <div className="col-3">
+                  <img
+                    src={deathsImg}
+                    alt="Deaths"
+                    className="img-fluid logo"
+                  />
+                </div>
+                <div className="col-8">
+                  <h2 className="text-danger">
+                    {data?.deaths ? (
+                      <CountUp
+                        start={0}
+                        end={data?.deaths}
+                        duration={2.3}
+                        separator={","}
+                      />
+                    ) : (
+                      <small className="text-primary">Loading . . .</small>
+                    )}
+                  </h2>
+                  <p className="text-muted">Deaths</p>
                 </div>
               </div>
-              <div className="col-md-6 col-sm-12 col-lg-12 my-3">
-                <div className="row deaths shadow align-items-center justify-content-between no-gutters">
-                  <div className="col-3">
-                    <img
-                      src={deathsImg}
-                      alt="Deaths"
-                      className="img-fluid logo"
-                    />
-                  </div>
-                  <div className="col-8">
-                    <h2 className="text-danger">
-                      {data?.deaths ? (
-                        <CountUp
-                          start={0}
-                          end={data?.deaths}
-                          duration={2.3}
-                          separator={","}
-                        />
-                      ) : (
-                        <small className="text-primary">Loading . . .</small>
-                      )}
-                    </h2>
-                    <p className="text-muted">Deaths</p>
-                  </div>
+            </div>
+            <div className="col-md-6 col-sm-12 col-lg-12 my-3">
+              <div className="row recovered shadow align-items-center justify-content-between no-gutters">
+                <div className="col-3">
+                  <img
+                    src={recoveredImg}
+                    alt="Recovered"
+                    className="img-fluid logo"
+                  />
                 </div>
-              </div>
-              <div className="col-md-6 col-sm-12 col-lg-12 my-3">
-                <div className="row recovered shadow align-items-center justify-content-between no-gutters">
-                  <div className="col-3">
-                    <img
-                      src={recoveredImg}
-                      alt="Recovered"
-                      className="img-fluid logo"
-                    />
-                  </div>
-                  <div className="col-8">
-                    <h2 className="text-success">
-                      {data?.recovered ? (
-                        <CountUp
-                          start={0}
-                          end={data?.recovered}
-                          duration={2.3}
-                          separator={","}
-                        />
-                      ) : (
-                        <small className="text-primary">Loading . . .</small>
-                      )}
-                    </h2>
-                    <p className="text-muted">Recovered</p>
-                  </div>
+                <div className="col-8">
+                  <h2 className="text-success">
+                    {data?.recovered ? (
+                      <CountUp
+                        start={0}
+                        end={data?.recovered}
+                        duration={2.3}
+                        separator={","}
+                      />
+                    ) : (
+                      <small className="text-primary">Loading . . .</small>
+                    )}
+                  </h2>
+                  <p className="text-muted">Recovered</p>
                 </div>
               </div>
             </div>
